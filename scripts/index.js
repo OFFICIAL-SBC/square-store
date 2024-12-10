5c1f5fbf90f1e17d6eec709929f9573ef0117359
OFFICIAL-SBC <sebastian.bonillac@udea.edu.co>
2024-12-09 22:08:56 -0500

    <feature>[Checkout page] Checkout page is done

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 let sideMenu = document.getElementsByClassName('aside__NavigationMenu')[0];
 let buttonOpenSideMenu = document.getElementsByClassName('header-above-part__menu-icon')[0];
 let buttonCloseSideMenu = document.getElementsByClassName('aside__close-icon')[0];
+let buttonMinicar= document.getElementById('minicar-icon');
+let asideMicartSection = document.getElementsByClassName('aside-container__shadow-box')[0];
+let buttonClosePopSideMenu = document.getElementById('close-popup-icon');
 
 buttonOpenSideMenu.addEventListener('click', function () {
     sideMenu.classList.toggle('aside__sideMenuOpened');
@@ -9,3 +12,11 @@ buttonOpenSideMenu.addEventListener('click', function () {
 buttonCloseSideMenu.addEventListener('click',function (){
     sideMenu.classList.toggle('aside__sideMenuOpened');
 });
+
+buttonMinicar.addEventListener('click', function (){
+    asideMicartSection.classList.toggle('aside-container__shadow-box--visible');
+});
+
+buttonClosePopSideMenu.addEventListener('click',function (){
+    asideMicartSection.classList.toggle('aside-container__shadow-box--visible');
+})
